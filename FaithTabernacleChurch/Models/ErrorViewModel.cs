753a3708bc9f6aa0f177544d15b690c63454bb44
mundas26 <Junielmundas26@gmail.com>
2023-10-26 12:58:36 +0800

    Add project files.

---
+++ FaithTabernacleChurch/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FaithTabernacleChurch.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
