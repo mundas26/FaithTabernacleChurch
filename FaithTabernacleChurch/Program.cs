472df7dcb8aab437f9bc2d760a47008daef6ee06
mundas26 <Junielmundas26@gmail.com>
2023-10-29 13:44:07 +0800

    Add Navbar & style

--- FaithTabernacleChurch/Program.cs
+++ FaithTabernacleChurch/Program.cs
@@ -1,8 +1,15 @@
+using Ftc.DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(option =>
+option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
---
+++ Ftc.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ftc.DataAccess.Data
+{
+    public class ApplicationDbContext: IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> option) : base(option) { }
+    }
+}
